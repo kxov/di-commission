e3ee5beb996a293dfa5c8e59bc1244beef337572
makx <makx.work@gmail.com>
2020-07-29 16:06:21 +0300

    Add ExchangeRateTest, CardBinTest

---
+++ tests/Unit/Domain/ExchangeRate/ExchangeRateTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Unit\Domain\ExchangeRate;
+
+use PHPUnit\Framework\TestCase;
+use Task\Domain\ExchangeRate\ExchangeRate;
+
+class ExchangeRateTest extends TestCase
+{
+    public function testSuccess()
+    {
+        $exchangeRate = new ExchangeRate($currency = 'USD', $rate = 1.1717);
+
+        $this->assertEquals($exchangeRate->getCurrency(), $currency);
+        $this->assertEquals($exchangeRate->getRate(), $rate);
+    }
+
+    public function testCreateWithNullRate()
+    {
+        $exchangeRate = new ExchangeRate($currency = 'USD', null);
+
+        $this->assertEquals($exchangeRate->getCurrency(),  $currency);
+        $this->assertEquals($exchangeRate->getRate(),  0.0);
+    }
+}
