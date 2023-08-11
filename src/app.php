57863d435dac4b8a76d1ccc022de9ee33d1a0a83
Maxim Trofimov <m.trofymov@maudau.tech>
2023-08-11 22:39:56 +0300

    add di config

--- src/app.php
+++ src/app.php
@@ -2,13 +2,9 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+$config = require_once __DIR__ . '/config.php';
 
-$container = new ContainerBuilder();
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__));
-$loader->load('services.yaml');
+$container = require_once __DIR__ . "/di/{$config['di']}.php";
 
 try {
     $reader = $container->get('reader');
---
+++ src/di/symfony.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+$container = new ContainerBuilder();
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__));
+$loader->load('services.yaml');
+
+$container->compile();
+
+return $container;
